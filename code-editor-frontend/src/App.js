c4f467ae6f64398e283095efe6f2f739c0a2b7ea
MohammadOsseily <mohammad.osseily2@gmail.com>
2024-07-28 21:20:33 +0300

    created navbar added tailwind theme color

--- code-editor-frontend/src/App.js
+++ code-editor-frontend/src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login/Login';
-import Register from './pages/Register/Register';
-import Admin from './pages/Admin/Admin';
-import EditUser from './pages/Admin/EditUser';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Admin from "./pages/Admin/Admin";
+import EditUser from "./pages/Admin/EditUser";
+import Home from "./pages/Home/page";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/admin' element={<Admin />} />
-          <Route path='/edit-user/:id' element={<EditUser />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/edit-user/:id" element={<EditUser />} />
         </Routes>
       </div>
     </Router>
--- code-editor-frontend/tailwind.config.js
+++ code-editor-frontend/tailwind.config.js
@@ -5,4 +5,20 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#6EDB52",
+          secondary: "#263340",
+          accent: "#0C0F14",
+          neutral: "#C9CCD1",
+          info: "#3abff8",
+          success: "#36d399",
+          warning: "#fbbd23",
+          error: "#f87272",
+        },
+      },
+    ],
+  },
 };

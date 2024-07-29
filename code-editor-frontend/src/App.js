67e32f05bbc83523f5fe443bb60744e083fceced
Alirh1 <alirhayem.1212@gmail.com>
2024-07-29 15:29:49 +0300

    modified styling for admin page and added navbar for all pages

--- code-editor-frontend/src/App.js
+++ code-editor-frontend/src/App.js
@@ -4,16 +4,31 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import Admin from './pages/Admin/Admin';
 import EditUser from './pages/Admin/EditUser';
+import CodeEditor from './pages/CodeEditor/CodeEditor';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
+import Home from './pages/Home/page'
+import Navebar from './components/Navbar';
 
 function App() {
   return (
     <Router>
       <div>
+        <Navebar/>
         <Routes>
+          <Route path='/home' element={<Home />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/admin' element={<Admin />} />
           <Route path='/edit-user/:id' element={<EditUser />} />
+          <Route 
+                        path="/editor" 
+                        element={
+                            <ChakraProvider theme={theme}>
+                                <CodeEditor />
+                            </ChakraProvider>
+                        } 
+                    />
         </Routes>
       </div>
     </Router>
